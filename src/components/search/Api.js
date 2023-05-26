ca40bc4c092ce40c2c3e5f870160946df8718023
Domains18 <gibsonsgibson88@gmail.com>
2023-05-26 13:12:00 +0300

    files reload
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/components/search/Api.js
+++
@@ -1,9 +0,0 @@
-export const geoApi = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': 'dae7246909msh4979a69348a89a1p1b5df7jsne874660aef4e',
-        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-    }
-};
-export const GEO_API_URL = " https://wft-geo-db.p.rapidapi.com/v1/geo"
-
