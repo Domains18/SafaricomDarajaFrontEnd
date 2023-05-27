ac13fcd2004e4506c9b7ff1e31ddc823ddb36cef
Domains18 <gibsonsgibson88@gmail.com>
2023-05-27 11:54:04 +0300

    score schema
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

---
+++ src/models/score.schema.ts
@@ -0,0 +1,35 @@
+import {Document,  Schema as MongooseSchema, Types } from 'mongoose';
+import { IScore } from 'src/types/interfaces/score';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+
+@Schema({ collection: 'scores' })
+export class Score extends Document implements IScore{
+    @Prop({
+        type: MongooseSchema.Types.ObjectId,
+        isRequired: true,
+        default: Types.ObjectId
+    })
+    _id: string;
+
+    @Prop({
+        ref: 'User',
+        type: MongooseSchema.Types.ObjectId,
+        isRequired: true
+    })
+    user: Types.ObjectId;
+
+    @Prop({
+        type: Number,
+        isRequired: true,
+        default: 0,
+        index: true,
+    })
+    score: number
+
+    @Prop({
+        type: Date,
+        default: Date.now,
+    })
+    createdAt?: Date
+}
