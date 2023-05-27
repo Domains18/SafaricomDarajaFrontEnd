87b5fdd3fd73adeb557a299ef195c09f7fd69c28
Domains18 <gibsonsgibson88@gmail.com>
2023-05-27 13:52:28 +0300

    resolved strategy
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/modules/auth/strategies/jwtStrategy.ts
+++ src/modules/auth/strategies/jwtStrategy.ts
@@ -1,3 +1,31 @@
 import { Injectable, UnauthorizedException } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { UserService } from '../../user/user.service'
+import { UserJwtPayload } from "src/types/interfaces/userjwtpayload";
+import { Strategy } from 'passport-jwt'
+import { IUser } from "src/types/interfaces/user";
+import { PassportStrategy } from "@nestjs/passport";
+import { Request } from "express";
+
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(
+        private configService: ConfigService,
+        private readonly userService: UserService,
+    ) {
+        super({
+            jwtFromRequest: (req: Request) => {
+                return req.cookies?.auth_token;
+            },
+            secretOrKey: configService.get('JWT_SECRET') || 'super-secret-key'
+        });
+    }
+    async validate(payload: UserJwtPayload): Promise<IUser> {
+        const targetUser = await this.userService.find(payload);
+        if (!targetUser) {
+            throw new UnauthorizedException();
+        }
+        return targetUser
+    }
+}
--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,16 +1,2 @@
-import { Controller, Get, Req, UseGuards } from "@nestjs/common";
-import { JwtAuthGuard } from '../../guards/jwtAuth.guard'
-import { AuthorizedRequest } from "src/types/authorizedRequests";
-import { USerInfoDto } from "src/types/dto/user/getUserDto";
-import { userService } from './user.service'
-
-
-@Controller('user')
-export class userController{
-    constructor(private readonly USerService: userService) { }
-    @Get()
-    @UseGuards(JwtAuthGuard)
-    async get(@Req() req: AuthorizedRequest): Promise<USerInfoDto>{
-        return await this.USerService.findOmitPassword(req.user)
-    }
-}
+import { Controller, Get, Req,  UseGuards} from "@nestjs/common";
+import { JwtAuthGuard } from ''
