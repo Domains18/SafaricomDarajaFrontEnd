4a8412fb7de5e8762ad40c60d89ed1316606b99b
Domains18 <gibsonsgibson88@gmail.com>
2023-05-27 19:49:44 +0300

    resolved controller
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,2 +1,16 @@
 import { Controller, Get, Req,  UseGuards} from "@nestjs/common";
-import { JwtAuthGuard }
+import { JwtAuthGuard } from "src/guards/jwtAuth.guard";
+import { AuthorizedRequest } from "src/types/authorizedRequests";
+import { USerInfoDto } from "src/types/dto/user/getUserDto";
+import { UserService } from './user.service';
+
+
+@Controller('user')
+export class UserController{
+    constructor(private readonly userService: UserService) { }
+    @Get()
+    @UseGuards(JwtAuthGuard)
+    async get(@Req() req: AuthorizedRequest): Promise<USerInfoDto>{
+        return await this.userService.findOmitPassword(req.user);
+    }
+}
