67eb98e996cab99ce8ae730313191cc22b4342e6
Domains18 <gibsonsgibson88@gmail.com>
2023-05-27 22:17:22 +0300

    score
    Signed-off-by: Domains18 <gibsonsgibson88@gmail.com>

---
+++ src/modules/score/score.service.ts
@@ -0,0 +1,40 @@
+import { ConflictException, Injectable } from "@nestjs/common";
+import { Model } from 'mongoose'
+import { Score } from "src/models/score.schema";
+import { createScoreRequestsDto } from "src/types/dto/scores/createScoreDto";
+import { InjectModel } from "@nestjs/mongoose";
+import { IUser } from "src/types/interfaces/user";
+import { IScore } from "src/types/interfaces/score";
+import { SortFilter } from "src/types/enums/sortFilters";
+import { User } from "src/models/user.schema";
+import { CollectionResponse } from "src/types/interfaces/collectionResponse";
+
+
+
+@Injectable()
+export class ScoreService {
+    constructor(
+        @InjectModel(Score.name) private readonly scoreModel: Model<Score>,
+        @InjectModel(User.name) private readonly userModel: Model<User>,
+    ) { }
+    async findAllByUser(
+        user: IUser,
+        sorter: SortFilter = SortFilter.DESCENDING,
+        page = 1,
+        limit = 100,
+    ): Promise<CollectionResponse<IScore>> {
+        const result = await this.scoreModel
+            .find({ user: user._id })
+            .sort({ score: sorter })
+            .skip(limit * (page - 1))
+            .limit(limit)
+            .exec();
+
+        return {
+            totalCount: await this.scoreModel.find({ user: user._id }).count(),
+            collection: result
+        }
+    }
+    
+
+}
--- src/types/dto/scores/createScoreDto.ts
+++ src/types/dto/scores/createScoreDto.ts
@@ -2,7 +2,7 @@ import { IScore } from "src/types/interfaces/score";
 import { IUser } from "src/types/interfaces/user";
 
 
-export type createScoreRequestsDton = Omit<IDBObjectStore, 'user'> & {
+export type createScoreRequestsDto = Omit<IDBObjectStore, 'user'> & {
     user: IUser;
 }
 export type createScoreResponseDto = Omit<IScore, 'user'> & {
