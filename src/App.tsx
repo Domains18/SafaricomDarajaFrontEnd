6356607befe593e69e66a0206dab1537d7234576
Domains18 <gibsonsgibson88@gmail.com>
2023-11-23 15:01:40 +0300

    Refactor Vite and Tailwind configuration files

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,55 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [amount, setAmount] = useState<string>('');
+  const [phoneNumber, setPhoneNumber] = useState<string>('');
+
+  const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPhoneNumber(e.target.value);
+  };
+
+  const handleAmountChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setAmount(e.target.value);
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log("Submitted:", phoneNumber, amount);
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App bg-slate-700 h-screen text-white w-full">
+      <header className="App-header">
+        <p className="text-center pt-9">Please Enter your Details Below</p>
+      </header>
+      <div className="flex items-center justify-center">
+        <form className="flex flex-col items-center" onSubmit={handleSubmit}>
+          <input
+            className="border-2 border-gray-500 rounded-md p-2 m-2 text-black"
+            type="number"
+            placeholder="Enter your Phone Number"
+            value={phoneNumber}
+            onChange={handlePhoneNumberChange}
+          />
+          <input
+            className="border-2 border-gray-500 rounded-md p-2 m-2 text-black"
+            type="text"
+            placeholder="Enter Amount"
+            value={amount}
+            onChange={handleAmountChange}
+          />
+          <button
+            className={`border-2 border-gray-500 rounded-md p-2 m-2 bg-green-600 
+            hover:bg-green-700 ${!phoneNumber || !amount ? 'opacity-50 cursor-not-allowed' : ''}`}
+            type="submit"
+            disabled={!phoneNumber || !amount}
+          >
+            Submit
+          </button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
