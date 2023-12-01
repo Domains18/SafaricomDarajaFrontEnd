362bf20682d2c0cbeca54399c28874b43dbd710e
Domains18 <gibsonsgibson88@gmail.com>
2023-12-01 17:25:23 +0300

    Refactor form submission and add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import axios from "axios";
+
+
 
 function App() {
-  const [amount, setAmount] = useState<string>('');
-  const [phoneNumber, setPhoneNumber] = useState<string>('');
+  const [amount, setAmount] = useState<string>("");
+  const [phoneNumber, setPhoneNumber] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPhoneNumber(e.target.value);
@@ -12,8 +16,14 @@ function App() {
     setAmount(e.target.value);
   };
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setIsLoading(true);
+
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+
+    setIsLoading(false);
+
     console.log("Submitted:", phoneNumber, amount);
   };
 
@@ -26,26 +36,34 @@ function App() {
         <form className="flex flex-col items-center" onSubmit={handleSubmit}>
           <input
             className="border-2 border-gray-500 rounded-md p-2 m-2 text-black"
-            type="number"
+            type="text"
             placeholder="Enter your Phone Number"
             value={phoneNumber}
             onChange={handlePhoneNumberChange}
           />
           <input
             className="border-2 border-gray-500 rounded-md p-2 m-2 text-black"
-            type="text"
+            type="number"
             placeholder="Enter Amount"
             value={amount}
             onChange={handleAmountChange}
           />
           <button
             className={`border-2 border-gray-500 rounded-md p-2 m-2 bg-green-600 
-            hover:bg-green-700 ${!phoneNumber || !amount ? 'opacity-50 cursor-not-allowed' : ''}`}
+            hover:bg-green-700 ${
+              !phoneNumber || !amount ? "opacity-50 cursor-not-allowed" : ""
+            }`}
             type="submit"
             disabled={!phoneNumber || !amount}
           >
             Submit
           </button>
+          {isLoading && (
+            <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
+              <p className="text-white absolute">Loading...</p>
+              <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
+            </div>
+          )}
         </form>
       </div>
     </div>
